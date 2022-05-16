ec76df9277cdf2fe7a728eb40079a7dd944dbbd1
dpr <rbourque64@gmail.com>
2022-05-16 10:27:10 -0400

    cleaned up main

--- payroll Partner/src/main/java/com/payrollpartner/Main.java
+++ payroll Partner/src/main/java/com/payrollpartner/Main.java
@@ -6,13 +6,10 @@ public class Main {
 	public static main.java.com.payrollpartner.databases.EmployeeDatabase employeeDatabase = new main.java.com.payrollpartner.databases.EmployeeDatabase();
 	public static int loggedinemp = -1;
 	public static int infoOn = 1; // 0 for no pannel, 1 for info pannl
-	
 	public static String compBanner = "Use your X company credentials to login.";
 	
-	public static void main(String[] args) {
-			
+	public static void main(String[] args) {	
 			new main.java.com.payrollpartner.userinterfaces.GuiManager();
-
 	}
 
 }
